ba57ea1910e0ca581306fbab08d6c240ae1a551e
Dwane Pennant <pennandw@corp.ads>
2018-11-17 10:13:48 -0500

    Added :id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); 
+var {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,6 +25,35 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(400).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            return res.status(400).send();
+    }
+
+   res.send({todo});
+
+    }).catch((e) => {
+        res.status(400).send();
+    }); 
+});
+
+module.exports = {app};
 
 /* app.post('/todos', (req, res) => { 
     var todo = new Todo({
